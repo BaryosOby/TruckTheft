48e79951b8abfd2a7e5405c1ce2e1af66ba7c471
Itamar <itamar463mm@gmail.com>
2022-06-10 18:11:07 +0300

    state trooper

--- StateTrooper.cpp
+++ StateTrooper.cpp
@@ -1,5 +1,30 @@
-//
-// Created by Itamar on 08/06/2022.
-//
 
 #include "StateTrooper.h"
+
+void StateTrooper::getClosestWarehouse() {
+    double min_dist = UINT16_MAX;
+    weak_ptr<Warehouse> curr_min;
+    for (const auto &p: warehouses) {
+        if (!p.second) {
+            if (min_dist > distance(getLocation(), p.first.lock()->getLocation())) {
+                min_dist = distance(getLocation(), p.first.lock()->getLocation());
+                tb.setDestination(p.first.lock()->getLocation());
+                curr_min = p.first;
+            } else if (min_dist == distance(getLocation(), p.first.lock()->getLocation())) {
+                string w1 = curr_min.lock()->getName();
+                string w2 = p.first.lock()->getName();
+                unsigned int length = min(w1.size(), w2.size());
+                for (int i = 0; i < length; ++i) {
+                    if (w1[i] == w2[i]) continue;
+                    else if (w1[i] < w2[i]) {
+                        break;
+                    } else {
+                        min_dist = distance(getLocation(), p.first.lock()->getLocation());
+                        tb.setDestination(p.first.lock()->getLocation());
+                        curr_min = p.first;
+                    }
+                }
+            }
+        }
+    }
+}
--- StateTrooper.h
+++ StateTrooper.h
@@ -4,10 +4,37 @@
 
 #ifndef CPPAD03_STATETROOPER_H
 #define CPPAD03_STATETROOPER_H
+
 #include "Vehicle.h"
 
 
-class StateTrooper : public Vehicle{
+class StateTrooper : public Vehicle {
+private:
+    vector<pair<weak_ptr<Warehouse>,bool> > warehouses;
+    Warehouse &start_warehouse;
+public:
+    StateTrooper(string &name, Warehouse &w) : Vehicle(name, w.getLocation().x, w.getLocation().y),
+                                               start_warehouse(w) {
+        Vehicle::state = stopped;
+        tb.setSpeed(90);
+    }
+
+    const vector<pair<weak_ptr<Warehouse>, bool>> &getWarehouses() const {
+        return warehouses;
+    }
+
+    void pushWarehouse(weak_ptr<Warehouse> &w) {
+        auto p = make_pair(w, false);
+        warehouses.push_back(p);
+    }
+
+    Warehouse &getStartWarehouse() const {
+        return start_warehouse;
+    }
+    void getClosestWarehouse();
+
+
+    void update(double general_time) override;
 
 };
 

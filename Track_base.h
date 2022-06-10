7438648041e78da775545ed7e0ed09b98877534d
Itamar <itamar463mm@gmail.com>
2022-06-10 09:49:15 +0300

    KJKJ

--- Track_base.h
+++ Track_base.h
@@ -9,26 +9,28 @@ using namespace std;
 
 class Track_base {
 private:
-    float course;
-    float speed;
+    double course;
+    double speed;
     Point destination;
     Point location;
 public:
 
-    float getCourse() const {
+    Track_base(double c, double s, double xD, double, double yD, double xL, double yL) : course(c), speed(s), destination(xD,yD), location(xL,yL){}
+
+    double getCourse() const {
         return course;
     }
 
-    void setCourse(float course) {
-        Track_base::course = course;
+    void setCourse(double rhs) {
+        Track_base::course = rhs;
     }
 
-    float getSpeed() const {
+    double getSpeed() const {
         return speed;
     }
 
-    void setSpeed(float speed) {
-        Track_base::speed = speed;
+    void setSpeed(double rhs) {
+        Track_base::speed = rhs;
     }
 
     const Point &getDestination() const {

0477f30c30e83ee95ff573e572b9be96d8c2220d
BaryosOby <omribaryos@gmail.com>
2022-06-11 15:27:46 +0300

    View

--- View.cpp
+++ View.cpp
@@ -1,5 +1,54 @@
-//
-// Created by baryos on 08/06/2022.
-//
-
 #include "View.h"
+
+
+Point View::normalize(const Point &p) const {
+    int x = p.x / (100 * size);
+    int y = p.y / (100 * size);
+    Point r(x, y);
+    return r;
+}
+
+map<Point, string> &View::objects_in_range() {
+    double range_x = size * zoom + pan.x * zoom * 100;
+    return ;
+}
+
+const Point &View::getPan() const {
+    return pan;
+}
+
+void View::setPan(const Point &p) {
+    View::pan = p;
+}
+
+double View::getZoom() const {
+    return zoom;
+}
+
+void View::setZoom(double zm) {
+    View::zoom = zm;
+}
+
+int View::getSize() const {
+    return size;
+}
+
+void View::setSize(int sze) {
+    View::size = sze;
+}
+
+View::View():pan(DEF_PAN, DEF_PAN), zoom(DEF_SCALE), size(DEF_SIZE) {
+}
+
+void View::pushObj(const shared_ptr<Sim_obj> &sp) {
+    objects.push_back(sp);
+}
+
+void View::clear() {
+    objects.clear();
+}
+
+void View::show() const {
+
+}
+
--- View.h
+++ View.h
@@ -1,14 +1,47 @@
-//
-// Created by baryos on 08/06/2022.
-//
-
 #ifndef EXC03_VIEW_H
 #define EXC03_VIEW_H
+#include "VehicleFactory.h"
+#include "Warehouse.h"
+#include <map>
+
+#define MIN_SIZE 6
+#define MAX_SIZE 30
+#define DEF_SIZE 25
+#define DEF_SCALE 2.0
+#define DEF_PAN 0.0
 
 
 class View {
+private:
+    Point pan;
+    double zoom;
+    int size;
+    vector<weak_ptr<Sim_obj>> objects;
 
+    Point normalize(const Point& p) const;
+    map<Point, string>& objects_in_range();
+
+public:
+    View();
+
+    const Point &getPan() const;
+
+    void setPan(const Point &pan);
+
+    double getZoom() const;
+
+    void setZoom(double zoom);
+
+    int getSize() const;
+
+    void setSize(int size);
+
+    void pushObj(const shared_ptr<Sim_obj>& sp);
+
+    void clear();
+
+    void show() const;
 };
 
 
-#endif //EXC03_VIEW_H
+#endif
